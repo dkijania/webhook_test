90cb4bc73da2bd680783b9d07c7525e565b500c3
dkijania <dariusz@o1labs.org>
2023-08-31 23:40:42 +0200

    fix script

--- .github/scripts/merges-cleanly.sh
+++ .github/scripts/merges-cleanly.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BRANCH=$1
+CURRENT=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}
 echo 'Testing for conflicts between the current branch `'"${GITHUB_HEAD_REF}"'` and `'"${BRANCH}"'`...'
 
 # Adapted from this stackoverflow answer: https://stackoverflow.com/a/10856937
@@ -10,14 +11,15 @@ echo 'Testing for conflicts between the current branch `'"${GITHUB_HEAD_REF}"'` 
 # Fetch a fresh copy of the repo
 git fetch origin
 # Check mergeability
-git merge-tree `git merge-base origin/$BRANCH HEAD` HEAD origin/$BRANCH | grep -A 25 "^+<<<<<<<"
+
+curl -s https://github.com/dkijania/webhook_test/branches/pre_mergeable/${BRANCH}...${CURRENT} | grep "Able to merge"
 
 RET=$?
 
 if [ $RET -eq 0 ]; then
   # Found a conflict
   echo "[ERROR] This pull request conflicts with $BRANCH, please open a new pull request against $BRANCH at this link:"
-  echo "https://github.com/dkijania/webhook_test/compare/${BRANCH}...${GITHUB_HEAD_REF}"
+  echo "https://github.com/dkijania/webhook_test/compare/${BRANCH}...${CURRENT}"
   exit 1
 else
   echo "No conflicts found against upstream branch ${BRANCH}"
